1fe59dcd9f2def33fe1600a4523d33d67ea82e96
Paul Brannan <curlypaul924@gmail.com>
2011-05-05 10:02:15 -0400

    Terminate argv with a null pointer.

--- ext/dash/dash.c
+++ ext/dash/dash.c
@@ -5,13 +5,18 @@ VALUE rb_dash_main(int argc, VALUE * argv, VALUE self)
 {
   int j;
   int result;
-  char * * c_argv = ALLOCA_N(char *, argc);
+  char * * c_argv = ALLOCA_N(char *, argc+1);
+  printf("%p\n", c_argv);
 
   for(j = 0; j < argc; ++j)
   {
     c_argv[j] = StringValueCStr(argv[j]);
+    printf("%d %s\n", j, c_argv[j]);
   }
 
+  c_argv[argc] = 0;
+
+  printf("calling dash_main - %p\n", c_argv);
   result = dash_main(argc, c_argv);
 
   return INT2NUM(result);
